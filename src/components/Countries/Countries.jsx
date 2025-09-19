845249bb85a0f159eda26d9bbed1678a69bed0ee
himel2535 <monwarhossanhimel@gmail.com>
2025-09-20 00:55:45 +0600

    data access by api call

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,15 +1,20 @@
-import React, { use } from 'react';
+import React, { use } from "react";
+import Country from "../Country/Country";
 
-const Countries = ({countriesPromise}) => {
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
 
-    const countriesData=use(countriesPromise);
-    const countries=countriesData.countries
-    console.log(countries)
-    return (
-        <div>
-            <h1>In the countries : {countries.length}</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>In the countries : {countries.length}</h1>
+      {countries.map((country) => (
+        <Country
+        key={country.cca3.cca3}
+        country={country}></Country>
+      ))}
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const Country = () => {
-    return (
-        <div>
-            <h3>Name:</h3>
-        </div>
-    );
+const Country = ({ country }) => {
+  console.log(country);
+
+  return (
+    <div>
+      <img src={country.flags.flags.png} alt="" />
+
+      <h3>Name :{country.name.common}</h3>
+
+      <h4>Capital : {country.capital.capital}</h4>
+
+      <p>Population : {country.population.population}</p>
+
+      <p>region : {country.region.region}</p>
+
+      {/* <h5>Languages : {country.languages.languages}</h5> */}
+
+      {/* <h5>Currencies : {country.currencies}</h5> */}
+    </div>
+  );
 };
 
 export default Country;
